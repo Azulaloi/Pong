b5e8b1817e3ca7274110c66681a6284577b75b22
Azulaloi Champagne <azulaloichampagne@gmail.com>
2017-08-10 18:52:20 -0400

    Initial Commit

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public GameObject ball;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+		if (!ball.activeSelf) {
+            MoveBall();
+        }
+	}
+
+    void MoveBall() {
+        Vector3 ballOrigin = new Vector3(0, 0, 0);
+        Quaternion ballRotation = Quaternion.identity;
+        //Instantiate(ball, ballOrigin, ballRotation);
+        ball.GetComponent<Transform>().SetPositionAndRotation(ballOrigin, ballRotation);
+        ball.AddComponent<BallHandler>();
+        ball.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+    public Camera cam;
+
+    public float speed = 0.1f;
+    public float moveSpeed = 0.1f;
+    public float moveTargetGoalY;
+    public float velocityYSmoothing;
+
+    Vector3 moveTarget;
+
+
+
+    // Use this for initialization
+    void Start () {
+	    if (!cam) {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveTargetGoalY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        moveTarget.y = Mathf.SmoothDamp(moveTarget.y, moveTargetGoalY, ref velocityYSmoothing, speed);
+        Move(moveTarget);
+	}
+
+    public void Move(Vector3 move){
+        Vector3 pos = transform.position;
+
+        transform.Translate(move);
+    }
+}
